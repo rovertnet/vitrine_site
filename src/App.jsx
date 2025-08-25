2aae53a0d0149413cb2fb6cd042923f328307491
rovertnet <matundukabamba@gmail.com>
2025-08-25 07:32:48 +0100

    fix: vercel routing + cache

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import FAQSection from './body/autresections/FAQSection';
 import PartnersMarquee from './body/autresections/PartnersMarquee';
 import EmailPopup from './body/autresections/EmailPopup';
 import NewsletterPopup from './footer/newsletter/NewsletterPopup';
+import TarifsServices from './body/tarif/TarifsServices';
 import { Toaster } from "react-hot-toast";
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
       <StatsSection />
       <ProcessTimeline />
       <AboutSection />
+      <TarifsServices />
       <TeamSection />
       <PortfolioSection />
       <TestimonialsEmbla />
